b2627858a079ec57be730760501498e09da8a48c
Prabhakar Bhat <prabhakarbhat@live.com>
2015-07-21 01:50:06 +0530

    Fixed db config for qa

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'd0980fc85353375542d7f361be39eebcf7c796df066fb14518d2ccc54c62d2d1330554a3f925a589396448eae0c6bfd77555d9a15c07479ed297364062312e6a'
+  config.secret_key = 'd0980fc85353375542d7f361be39eebcf7c796df066fb14518d2ccc54c62d2d1330554a3f925a589396448eae0c6bfd77555d9a15c07479ed297364062312e6a'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
