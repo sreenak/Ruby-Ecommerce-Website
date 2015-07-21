404569890c7ca4b177a24bb42edffd690aa6a27b
Amit Paul <amitp@langoor.com>
2015-07-21 17:43:42 +0530

    fabric colors modify

--- app/models/fabric_color.rb
+++ app/models/fabric_color.rb
@@ -3,4 +3,8 @@ class FabricColor < ActiveRecord::Base
   has_and_belongs_to_many :parts_groups
   validates_presence_of :swatch
   mount_uploader :swatch, ImageUploader
+
+  def label
+    "#{fabric.name} - #{name}"
+  end
 end
