db31c2f44caa20a7b80fdb18895c8145016f6b14
sreena <sreena@langoor.com>
2015-07-27 09:27:49 +0000

    applying search change

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
-   @q = Product.ransack(params[:q])
-   @products = @q.result(distinct: true)
+    q = params[:q]
+    @key=q
+    @trends   = Post.search(title_cont: q).result
+    @products = Product.search(name_or_description_cont: q).result
   end
 end
