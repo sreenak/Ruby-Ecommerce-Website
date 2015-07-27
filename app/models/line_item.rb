3fde11178932dc8e627bede36c67562ada5c29a1
sreena <sreena@langoor.com>
2015-07-27 04:19:59 +0000

    applying new changes

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :line_itemable, polymorphic: true
   belongs_to :discount_coupon, polymorphic: true, foreign_key: 'line_itemable_id', foreign_type: 'line_itemable_type', class_name: 'DiscountCoupon', counter_cache: :usage_count
+
   belongs_to :shipping
   has_one :shipping_address, through: :shipping, dependent: :destroy
   has_one :customised_dress_order_item
