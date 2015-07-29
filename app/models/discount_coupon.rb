a6246e41bf3a1e8386256e746a0766de1aa3783d
Prabhakar Bhat <prabhakarbhat@live.com>
2015-07-29 12:15:01 +0530

    Fixed discount maximum usages bug

--- app/models/discount_coupon.rb
+++ app/models/discount_coupon.rb
@@ -6,6 +6,6 @@ class DiscountCoupon < ActiveRecord::Base
   monetize :minimum_order_price_paisas, with_model_currency: :currency
 
   def valid_coupon?
-    active? and (maximum_usages == nil or maximum_usages >= usage_count)
+    active? and (maximum_usages == nil or maximum_usages > usage_count)
   end
 end
