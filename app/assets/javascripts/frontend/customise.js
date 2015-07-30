96836647c3378a4b5d484639a2e041d6071d404a
Dileep <dileep@langoor.com>
2015-07-30 13:57:43 +0530

    embellishment apply on svg is fixed

--- app/assets/javascripts/frontend/customise.js
+++ app/assets/javascripts/frontend/customise.js
@@ -556,10 +556,11 @@ $(window).load(function() {
 
                         };
                     };
-
+                    console.log(finalBrocadesArray);
 
                     for (var k = 0; k < finalBrocadesArray.length; k++) {
                         $('.defsclass').append("<svg><pattern id='img_brocade_" + finalBrocadesArray[k].brocadeId + "_" + finalBrocadesArray[k].partname + "' patternContentUnits='objectBoundingBox' viewBox='0 0 1 1' width='100%' height='100%' preserveAspectRatio='xMidYMid slice'><image preserveAspectRatio='xMidYMid slice' xlink:href=" + finalBrocadesArray[k].base64 + " width='1' height='1' /></pattern></svg>");
+                        //$('.defsclass').append("<svg><pattern id='img_brocade_" + finalBrocadesArray[k].brocadeId + "_" + finalBrocadesArray[k].partname + "' patternUnits='userSpaceOnUse' x='0' y='0'  width=" + finalBrocadesArray[k].width + " height=" + finalBrocadesArray[k].height + " preserveAspectRatio='xMidYMid slice'><image preserveAspectRatio='xMidYMid slice' xlink:href=" + finalBrocadesArray[k].base64 + " width=" + finalBrocadesArray[k].width + " height=" + finalBrocadesArray[k].height + " /></pattern></svg>");
                         $('.main_parts').find('path').each(function(index) {
                             if ($(this).attr('class') == finalBrocadesArray[k].partname) {
 
@@ -571,6 +572,7 @@ $(window).load(function() {
 
 
                 });
+
 
             } else {
                 for (var k = 0; k < finalBrocadesArray.length; k++) {
