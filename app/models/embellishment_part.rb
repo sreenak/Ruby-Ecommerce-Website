801faaba5101284cc1c097259e88dd290373d37b
Prabhakar Bhat <prabhakarbhat@live.com>
2015-07-30 13:34:55 +0530

    Updated dependents for embellishment

--- app/models/embellishment_part.rb
+++ app/models/embellishment_part.rb
@@ -1,6 +1,6 @@
 class EmbellishmentPart < ActiveRecord::Base
-  belongs_to :embellishment, dependent: :destroy
-  belongs_to :part, dependent: :destroy
+  belongs_to :embellishment
+  belongs_to :part
 
   validates_presence_of :embellishment, :image
 
