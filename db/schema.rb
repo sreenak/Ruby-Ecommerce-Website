b2cb6447ec04a66ddcd7e4f53b594a5c3f313680
Prabhakar Bhat <prabhakarbhat@live.com>
2015-07-31 15:33:54 +0530

    Fixed indentation

--- db/schema.rb
+++ db/schema.rb
@@ -248,7 +248,7 @@ ActiveRecord::Schema.define(version: 20150731061637) do
     t.string   "card_type",        limit: 255,   default: "",    null: false
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
-    t.string   "email",            limit: 255,                   null: false
+    t.string   "email",            limit: 255
   end
 
   create_table "likes", force: :cascade do |t|
@@ -305,7 +305,7 @@ ActiveRecord::Schema.define(version: 20150731061637) do
     t.datetime "updated_at",                              null: false
   end
 
-  add_index "parts", ["parts_group_id"], name: "index_parts_on_parts_group_id", using: :btree
+  add_index "parts", ["parts_group_id"], name: "index_parts_on_part_group_id", using: :btree
 
   create_table "parts_groups", force: :cascade do |t|
     t.string   "name",         limit: 255, default: "", null: false
@@ -448,7 +448,7 @@ ActiveRecord::Schema.define(version: 20150731061637) do
     t.string   "image",                  limit: 255
     t.string   "mobile",                 limit: 255
     t.date     "date_of_birth"
-    t.integer  "gender",                 limit: 2
+    t.integer  "gender",                 limit: 1
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
@@ -466,6 +466,9 @@ ActiveRecord::Schema.define(version: 20150731061637) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "auth_identities", "users"
+  add_foreign_key "brocade_parts", "brocades"
+  add_foreign_key "brocade_parts", "parts"
   add_foreign_key "customised_dresses", "dresses"
   add_foreign_key "customised_dresses", "users"
   add_foreign_key "dresses", "categories"
