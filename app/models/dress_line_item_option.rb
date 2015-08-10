dfb3202b3563c225d7428ce82e72821193143a68
Amit Paul <amitp@langoor.com>
2015-08-10 18:51:51 +0530

    order items in admin panel

--- app/models/dress_line_item_option.rb
+++ app/models/dress_line_item_option.rb
@@ -1,3 +1,4 @@
 class DressLineItemOption < ActiveRecord::Base
   belongs_to :line_item
+  belongs_to :standard_size
 end
--- app/models/product_line_item_option.rb
+++ app/models/product_line_item_option.rb
@@ -1,3 +1,4 @@
 class ProductLineItemOption < ActiveRecord::Base
   belongs_to :line_item
+  belongs_to :standard_size
 end
