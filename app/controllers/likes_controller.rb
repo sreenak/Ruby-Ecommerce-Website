6b7d3c65611d1011393b4bce0b42cea027ba5bc3
sreena <sreena@langoor.com>
2015-08-17 05:54:42 +0000

    applying new modifictions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   before_action :authenticate_user!, except: :create
 
   def index
-    @likes = current_user.likes.order(id: :desc).page(params[:page]).per(12)
+    @likes = current_user.likes.order(id: :desc).page(params[:page]).per(2)
   end
 
   def create
