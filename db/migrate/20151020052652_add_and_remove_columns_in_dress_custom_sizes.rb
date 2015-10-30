8be7d0da54a464e2fbcdcad3147c964b01f45c02
Amit Paul <amitp@langoor.com>
2015-10-30 12:56:16 +0530

    modified customdressessizes migration

--- db/migrate/20151020052652_add_and_remove_columns_in_dress_custom_sizes.rb
+++ db/migrate/20151020052652_add_and_remove_columns_in_dress_custom_sizes.rb
@@ -10,23 +10,23 @@ class AddAndRemoveColumnsInDressCustomSizes < ActiveRecord::Migration
       add_column :dress_custom_sizes, :around_neck, :integer
       add_column :dress_custom_sizes, :sleev_length, :integer
       add_column :dress_custom_sizes, :line_item_id, :integer
-      remove_column :dress_custom_sizes, :size, :string
-      remove_column :dress_custom_sizes, :name, :string
-      remove_column :dress_custom_sizes, :unit, :string
+      # remove_column :dress_custom_sizes, :size, :string
+      # remove_column :dress_custom_sizes, :name, :string
+      # remove_column :dress_custom_sizes, :unit, :string
   end
-  def down
-  	 remove_column :dress_custom_sizes, :bust_chest, :integer
-	  remove_column :dress_custom_sizes, :waist, :integer
-	  remove_column :dress_custom_sizes, :waist_to_ankle, :integer
-	  remove_column :dress_custom_sizes, :hips, :integer
-	  remove_column :dress_custom_sizes, :shoulder, :integer
-	  remove_column :dress_custom_sizes, :shoulder_to_neck, :integer
-	  remove_column :dress_custom_sizes, :around_armpit, :integer
-	  remove_column :dress_custom_sizes, :around_neck, :integer
-	  remove_column :dress_custom_sizes, :sleev_length, :integer
-	  remove_column :dress_custom_sizes, :line_item_id, :integer
-	  add_column :dress_custom_sizes, :size, :string
-	  add_column :dress_custom_sizes, :name, :string
-	  add_column :dress_custom_sizes, :unit, :string
-  end
+  # def down
+  # 	 remove_column :dress_custom_sizes, :bust_chest, :integer
+	 #  remove_column :dress_custom_sizes, :waist, :integer
+	 #  remove_column :dress_custom_sizes, :waist_to_ankle, :integer
+	 #  remove_column :dress_custom_sizes, :hips, :integer
+	 #  remove_column :dress_custom_sizes, :shoulder, :integer
+	 #  remove_column :dress_custom_sizes, :shoulder_to_neck, :integer
+	 #  remove_column :dress_custom_sizes, :around_armpit, :integer
+	 #  remove_column :dress_custom_sizes, :around_neck, :integer
+	 #  remove_column :dress_custom_sizes, :sleev_length, :integer
+	 #  remove_column :dress_custom_sizes, :line_item_id, :integer
+	 #  add_column :dress_custom_sizes, :size, :string
+	 #  add_column :dress_custom_sizes, :name, :string
+	 #  add_column :dress_custom_sizes, :unit, :string
+  # end
 end
