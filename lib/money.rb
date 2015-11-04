15ebeadf6f50b12c01bc5ef73e8b6d4fb8a70ea7
Prabhakar Bhat <prabhakarbhat@live.com>
2015-11-04 15:51:50 +0530

    Rounding exchanges

--- lib/money.rb
+++ lib/money.rb
@@ -3,6 +3,7 @@ class Money
 
   def exchange_to(other_currency, &rounding_method)
     value = old_exchange_to(other_currency, &rounding_method)
-    Money.new(value.to_i, other_currency)
+    Rails.logger.info 'Value: ' + value.to_s
+    Money.new(((value.to_d/100).ceil) * 100, other_currency)
   end
 end
